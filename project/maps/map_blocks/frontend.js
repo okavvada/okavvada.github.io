8dd8ead935102a78be94b656380ef4c075a16175
Olga Kavvada <okavvada@gmail.com>
2018-05-25 12:28:20 -0700

    json

---
+++ project/maps/map_blocks/frontend.js
@@ -0,0 +1,38 @@
+var map;
+
+// Main function that runs with the html page
+function initMap() {
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+      lat: 30.283747,
+      lng: -97.706758},
+        zoom: 10,
+      }); 
+
+
+	polyLayer = new google.maps.Data({map:map});
+	polyLayer.addGeoJson('zipcode_water.geojson');
+
+
+	polyLayer.setStyle(function(feature) {
+      if (feature.getProperty('total_water_MGD') > 1000000){
+        color = '#165906';
+      }
+      if (feature.getProperty('total_water_MGD') > 100000 && feature.getProperty('total_water_MGD') < 1000000){
+        color = '#219904';
+      }
+      if (feature.getProperty('total_water_MGD') > 1000 && feature.getProperty('total_water_MGD') < 100000) {
+        color = '#9df28a';
+      }
+      if (feature.getProperty('total_water_MGD') < 1000){
+        color = '#ffe554';
+      }
+      return ({visible: vis_biomass,
+              fillColor: color,
+              fillOpacity: 0.5,
+              strokeWeight: 0.3,
+              cursor: 'auto'})
+      })
+
+};
