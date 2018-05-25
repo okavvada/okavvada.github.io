dba223af0c01c4dc412c9f96794f90b3b3696076
Olga Kavvada <okavvada@gmail.com>
2018-05-25 11:19:32 -0700

    google maps

---
+++ project/frontend.js
@@ -0,0 +1,15 @@
+var map;
+
+// Main function that runs with the html page
+function initMap() {
+
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+      lat: 30.283747,
+      lng: -97.706758},
+        zoom: 10,
+        //scene: 'scene.yaml',
+      }); 
+
+  // Initialize the map to center to CA
+}
